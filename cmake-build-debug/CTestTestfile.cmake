6bbb19d311747e486162280f2aa0194ec23dc380
Piotr <piotr.nogalski70364@gmail.com>
2022-11-15 11:43:41 +0100

    Fixing compilation problem

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: C:/Programowanie/projekty_cpp/projekty_wlasne/Minesweeper
+# Build directory: C:/Programowanie/projekty_cpp/projekty_wlasne/Minesweeper/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(tests "sample_test")
+set_tests_properties(tests PROPERTIES  _BACKTRACE_TRIPLES "C:/Programowanie/projekty_cpp/projekty_wlasne/Minesweeper/CMakeLists.txt;31;add_test;C:/Programowanie/projekty_cpp/projekty_wlasne/Minesweeper/CMakeLists.txt;0;")
+subdirs("_deps/googletest-build")
