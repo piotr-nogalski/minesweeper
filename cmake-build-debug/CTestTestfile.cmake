5246398ca91958574ad716340fc789f0dde46142
Piotr Nogalski <111290102+piotr-nogalski@users.noreply.github.com>
2022-11-15 12:39:54 +0100

    Launch on Clion | Changes in mani.cpp

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: C:/Programowanie/mini_games/Minesweeper
+# Build directory: C:/Programowanie/mini_games/Minesweeper/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(tests "sample_test")
+set_tests_properties(tests PROPERTIES  _BACKTRACE_TRIPLES "C:/Programowanie/mini_games/Minesweeper/CMakeLists.txt;31;add_test;C:/Programowanie/mini_games/Minesweeper/CMakeLists.txt;0;")
+subdirs("_deps/googletest-build")
